11b6b3737507490598c2545c74b72c3331e728cd
Djavan <dejotadeveloper@gmail.com>
2025-05-23 23:23:10 -0300

    Adicionando menu lateral versão mobile

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,19 +1,72 @@
+import MenuIcon from '@mui/icons-material/Menu';
+import Close from '@mui/icons-material/Close';
+import { useState } from 'react';
+// import CloseIcon from '@mui/icons-material/Close';
+
 export default function Header(){
+
+    const [menuOpen, setMenuOpen] = useState(false)
+    console.log(menuOpen)
+
+
     return(
-        <div className="h-[5vh] md:h-[10vh] w-full px-4 md:px-20 flex justify-between items-center bg-[#222831] text-[#D8D7BE] md:font-semibold">
-            <span className="text-lg md:text-4xl text-[#00A6C0]">
-                <a href="/">
-                    TMB FÁCIL
-                </a>
-            </span>
-            <nav className="md:flex gap-4 text-sm font-semibold md:text-xl md:gap-8 hidden">
-                <span>
+        <>
+            <div className="h-[5vh] md:h-[10vh] w-full px-4 md:px-20 flex justify-between items-center bg-[#222831] text-[#E5E5E5] md:font-semibold">
+                <span className="text-lg md:text-4xl text-[#00A6C0]">
+                    <a href="/">
+                        TMB FÁCIL
+                    </a>
+                </span>
+                <nav className="md:flex gap-4 text-sm font-semibold md:text-xl md:gap-8 hidden">
+                    <span>
+                        <a href="/">
+                            INÍCIO 
+                        </a>
+                    </span>
+                    <span>
+                        <a href="/calculadora">
+                            CALCULADORA
+                        </a>
+                    </span>
+                </nav>
+                <span 
+                    class="absolute right-2 md:hidden"
+                    onClick={() => setMenuOpen(!menuOpen)}
+                >
+                    {!menuOpen && <MenuIcon className="" />}
+                    {menuOpen && <Close className="" />}
+
+                </span>
+            </div>
+            <nav className={`
+                absolute z-10 top-[5vh] right-0 w-1/2 min-h-[100dvh] bg-[#E5E5E5]
+                flex flex-col gap-0 border-l-2 border-[#C5C5C5]
+                transition-all duration-75
+                md:hidden
+                ${menuOpen ? 'translate-x-[0%]' : 'translate-x-[100%]'}
+            `}>
+                <span className='w-full px-4 py-1 border-b-2 border-[#222831]/20 text-[#222831]'>
                     <a href="/">
                         INÍCIO 
                     </a>
                 </span>
-                <span> CALCULADORA </span>
+                <span className='w-full px-4 py-1 border-b-2 border-[#222831]/20 text-[#222831]'>
+                    <a href="/calculadora">
+                        CALCULADORA
+                    </a>
+                
+                </span>
             </nav>
-        </div>
+            <div 
+                className={`
+                        absolute z-10 top-[5vh] left-0 w-1/2 min-h-[100dvh] bg-[#121212]
+                        duration-75
+                        md:hidden
+                        ${menuOpen ? 'opacity-50' : 'opacity-0'}
+                `}
+                onClick={() => setMenuOpen(!menuOpen)}>
+
+            </div>
+        </>
     )
 }
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,7 +5,7 @@ export default function Home(){
             {/* BANNER */}
             <main className="relative w-full min-h-[50vh] md:min-h-[90vh] bg-[url(./prato01.jpg)]  md:bg-right bg-contain bg-top md:bg-contain  bg-no-repeat flex">
                 <div className="
-                        w-full min-h-full py-4 md:p-0 md:m-0 bg-linear-to-t from-[#D8D7BE] from-45% to-transparent to-75%
+                        w-full min-h-full py-4 md:p-0 md:m-0 bg-linear-to-t from-[#E5E5E5] from-45% to-transparent to-75%
                         flex flex-col justify-end gap-4
                         md:relative md:justify-center md:gap-8 md:bg-linear-to-r md:from-45%
                 ">
@@ -20,15 +20,18 @@ export default function Home(){
                         href="/calculadora" 
                         className="md:w-1/2 h-fit py-4"
                     > 
-                        <span className="mx-8 px-4 py-2  border-transparent rounded-xl uppercase text-[#D8D7BE] font-semibold bg-[#222831]">
+                        <span className="mx-8 px-4 py-2  border-transparent rounded-xl uppercase text-[#E5E5E5] font-semibold bg-[#222831]">
                             Calcule sua TMB e comece agora!
                         </span>
                     </a>
                 </div>
             </main>
             <section className="relative w-full min-h-screen flex justify-center bg-[#283B48]">
-                <article className="w-full md:w-2/3 text-sm md:text-base flex flex-col items-center gap-12 py-8 px-12 bg-[#D8D7BE]">
-                    <span className="text-3xl font-bold font-sans"> O QUE É TMB?</span>
+                <article className="
+                        w-full  text-sm  flex flex-col items-center gap-12 py-4 px-8  bg-[#E5E5E5]
+                        md:w-2/3 md:text-base md:px-12 md:py-8
+                ">
+                    <span className="text-2xl md:text-3xl font-bold font-sans"> O QUE É TMB?</span>
                     <p>
                         TMB é taxa metabólica basal, representa a quantidade de energia que o organismo consome para manter suas funções 
                         essenciais enquanto está em descanso, como a respiração, os batimentos cardíacos e a regulação da temperatura 
@@ -44,10 +47,10 @@ export default function Home(){
                         ganhar massa ou perder gordura</b>.
                         Usando <a href="/calculadora" className="font-semibold underline text-[#283B48]">nossa calculadora</a> você descobre sua TMB e seu gasto energético, assim podendo controlar melhor o que come.
                     </p>
-                    <span className="w-full text-xl font-semibold font-sans"> Como faço para saber quanto estou comendo? </span>
+                    <span className="w-full text-xl md:text-2xl font-semibold font-sans"> Como faço para saber quanto estou comendo? </span>
                     <p>
-                        Você pode usar o aplicativo MyFitnessPal para registrar seus alimentos e refeições, lá você pode buscar 
-                        alimentos, criar receitas e até deixar suas refeições prontas.
+                        Existe alguns aplicativos para você buscar alimentos, criar receitas e até deixar suas refeições prontas,
+                        registrando numa espécie de diário. O que eu uso é o MyFitnessPal
                     </p>
                 </article>
             </section>
