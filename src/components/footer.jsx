26987efb4884ebbd23994cd493b28b776e413343
Djavan <dejotadeveloper@gmail.com>
2025-06-25 19:59:28 -0300

    Adicionando logomarca

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,7 +4,11 @@ export default function Footer(){
             <div className="min-h-[5vh] w-full px-4 py-2 flex flex-col justify-between items-center text-sm  bg-[#222831] text-[#E5E5E5] 
                             md:h-[10vh] md:px-20 md:flex-row md:justify-center md:gap-24 md:text-lg md:font-light           
             ">
-                <span className="w-auto h-auto px-1 py-0.5 md:px-2 md:py-1 rounded-md md:rounded-2xl text-lg md:text-2xl bg-[#00A6C0]"> TMB </span>
+                <span className="max-w-24 max-h-16 px-4 py-0.5 md:px-0 md:py-0 rounded-md md:rounded-2xl">
+                    <a href="/" className="">
+                        <img src="./LogoTMB.png" className="max-x-16 max-h-12 md:max-w-24 md:max-h-16" alt="Logomarca"/>
+                    </a>
+                </span>
                 <nav className="w-1/2 md:w-2/3 flex-col"> 
                     <ul className="w-full flex justify-center flex-wrap md:gap-12 font-semibold">
                         <li className="hover:text-[#00a6c0] hover:cursor-pointer hover:underline">
