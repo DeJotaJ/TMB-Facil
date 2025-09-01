576e8112d879f6d0bb06bed9a3e5307bf64f6b41
Djavan <dejotadeveloper@gmail.com>
2025-09-01 12:23:31 -0300

    Atualizando toda a home 1.0

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -28,7 +28,7 @@ export default function Home(){
                         href="/calculadora" 
                         className="lg:w-fit h-fit py-1 px-4 md:px-4 lg:px-8"
                     > 
-                        <span className="mx-4 px-2 py-0.5 border-transparent rounded-xl uppercase text-[#E7E7E7] font-semibold bg-[#121224]
+                        <span className="mx-4 px-3 py-1 border-transparent rounded-xl uppercase text-[#E7E7E7] font-semibold bg-[#121224]
                                          lg:hover:px-5 lg:hover:py-2.5 lg:hover:text-[17px]
                                          md:shadow-[0_2px_8px] md:shadow-[#060612]
                                          max-[420px]:text-sm
@@ -43,12 +43,78 @@ export default function Home(){
             {/* BANNER */}
             <section className="relative w-full min-h-screen flex justify-center  bg-[#E7E7E7] text-[#121224]">
                 <article className="
-                        w-full  text-sm  flex flex-col items-center gap-2 py-1 px-1 bg-[#E0E0E0] md:bg-[#D0D0D0]
+                        w-full  text-sm  flex flex-col items-center gap-2 py-1 px-1 bg-[#D0D0D0] md:bg-[#D0D0D0]
                         md:w-3/4 md:text-base md:px-2 md:py-2
                         lg:w-2/3 lg:text-base lg:px-3 lg:py-3
                 ">
+                    
+                    <div className="px-2 pt-4 pb-2 rounded-lg flex flex-col gap-3 md:px-4 md:pt-2 md:pb-4 md:flex md:flex-col md:gap-5 md:rounded-xl bg-[#E0E0E0]">
+                        {/* NUTRIÇÃO */}
+                        <div className="flex flex-col">
+                            <h2 className="text-2xl md:text-3xl font-bold">NUTRIÇÃO</h2>
+                            <span className="text-base">
+                                Nutrição é o processo de fornecer ao corpo os nutrientes de que ele precisa para funcionar corretamente. Ela 
+                                envolve o que comemos, como nosso corpo digere e como usa esses nutrientes.
+                            </span>
+                        </div>
+                        {/* FUNDAMENTOS DA NUTRIÇÃO */}
+                        <div className="flex flex-col">
+                            <h2 className="text-lg md:text-2xl font-bold">Fundamentos da Nutrição<span className="hidden md:block">: o que você realmente precisa saber para se alimentar melhor</span></h2>
+                            <span className="text-base">
+                                A base de uma vida saudável começa com entender como os alimentos funcionam no seu corpo.
+                            </span>
+                        </div>
+                        <div className="flex flex-col md:flex-row md:flex-wrap md:gap-y-6 md:gap-x-3">
+                            <div className="flex flex-col items-start md:flex-row md:justify-center md:gap-3 md:flex-wrap">
+                                <span className="w-full lg:w-[22.5%] mt-3 font-semibold order-0 lg:order-0">🌱 Macronutrientes</span>
+                                <span className="w-full lg:w-[22.5%] mt-3 font-semibold order-2 lg:order-0">🍊 Micronutrientes</span>
+                                <span className="w-full lg:w-[22.5%] mt-3 font-semibold order-4 lg:order-0">💧 Água</span>
+                                <span className="w-full lg:w-[22.5%] mt-3 font-semibold order-6 lg:order-0">🌾 Fibras</span>
+                                <ul className="lg:w-[22.5%] px-1 order-1 lg:order-0">
+                                    <li className="list-disc ml-4">O combustível do corpo: carboidratos, proteínas e gorduras.</li>
+                                    <li className="list-disc ml-4">Cada um tem um papel único no seu dia a dia.</li>
+
+                                </ul>
+                                <ul className="lg:w-[22.5%] px-1 order-3 lg:order-0">
+                                    <li className="list-disc ml-4">As “vitaminas e minerais” que regulam tudo dentro de você.</li>
+                                    <li className="list-disc ml-4">Pequenos em quantidade, enormes em importância.</li>
+
+                                </ul>
+                                <ul className="lg:w-[22.5%] px-1 order-5 lg:order-0">
+                                    <li className="list-disc ml-4">Mais do que matar a sede: essencial para energia, foco e saúde.</li>
+
+                                </ul>
+                                <ul className="lg:w-[22.5%] px-1 order-7 lg:order-0">
+                                    <li className="list-disc ml-4">Seu intestino agradece.</li>
+                                    <li className="list-disc ml-4">Controlam a digestão e ajudam até no controle de peso.</li>
+
+                                </ul>
+                            </div>
+                            <div className="flex flex-col items-start md:flex-row md:justify-center md:gap-3 md:flex-wrap">
+                                <span className="lg:w-[45%] mt-3 font-semibold order-0 md:order-0">🔥 Energia e Calorias</span>
+                                <span className="lg:w-[45%] mt-3 font-semibold order-2 md:order-0">🥗 Alimentação Equilibrada</span>
+                                <ul className="lg:w-[45%] px-1 order-1 md:order-0">
+                                    <li className="list-disc ml-4">Entenda como funciona o “saldo calórico”.</li>
+                                    <li className="list-disc ml-4">O segredo por trás de perder, manter ou ganhar peso.</li>
+
+                                </ul>
+                                <ul className="lg:w-[45%] px-1 order-3 md:order-0">
+                                    <li className="list-disc ml-4">Não é dieta restrita.</li>
+                                    <li className="list-disc ml-4">É aprender a montar pratos inteligentes que sustentam o corpo e a mente.</li>
+
+                                </ul>
+                            </div>
+                            <div className="flex flex-col mt-2 md:m-0 gap-3 lg:gap-2">
+                                <span className="text-base font-medium md:font-semibold">Quer entender de forma simples e prática como aplicar tudo isso no seu dia a dia?</span>
+                                <span className="w-fit rounded-lg bg-[#121224] px-2 py-1 text-[#e7e7e7]">
+                                    Descubra os Fundamentos da Nutrição →
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+
                     {/* EXPLICAÇÃO TMB */}
-                    <div className="px-2 py-1 rounded-lg md:px-2 md:py-2 md:rounded-xl bg-[#E0E0E0]">
+                    <div className="px-2 pt-1 pb-2 rounded-lg  md:px-4 md:py-2 md:flex md:flex-col md:gap-5 md:rounded-xl bg-[#E0E0E0]">
                         <h2 className="text-2xl md:text-3xl text-center font-bold font-sans mb-4 md:mb-8"> O QUE É TMB?</h2>
                         <p className="w-full h-auto flex flex-col gap-4 mb-8 md:mb-4">
                             <span>
@@ -70,25 +136,88 @@ export default function Home(){
                         {/* PORQUE CALCULAR SUA TMB? */}
                         <h2 className="w-full text-xl md:text-2xl font-semibold font-sans mb:0.5 md:mb-2"> Por que calcular sua TMB? </h2>
                         <p className="w-full h-auto mb-4">
-                            <ul className="list-disc list-inside pl-2 flex flex-col gap-2 md:gap-1">
+                            <ul className="list-disc list-inside pl-2 flex flex-col gap-1 md:gap-1">
                                 <li> Evita dietas aleatórias</li>
                                 <li> Ajuda a planejar sua alimentação</li>
                                 <li> Reduz erros comuns ao tentar emagrecer ou ganhar massa</li>
                                 <li> Promove saúde com responsabilidade</li>
                             </ul>
                         </p>
-                    </div>
-                    
-                    {/* COMO CALCULAR COMIDA */}
-                    <div className="px-2 py-1 rounded-lg md:px-2 md:py-2 md:rounded-xl bg-[#E0E0E0]">
-                        <span className="w-full text-xl md:text-2xl font-semibold font-sans mb:0.5 md:mb-1"> Como saber quanto estou comendo? </span>
+                        <span className="w-full text-xl md:text-2xl font-semibold font-sans mb:2 md:mb-1"> Como saber quanto estou comendo? </span>
                         <p className="w-full h-auto mb-4">
                             Existem aplicativos que facilitam o controle da alimentação diária. Com eles, você pode buscar alimentos, 
                             escanear rótulos, salvar receitas e registrar tudo o que come ao longo do dia.
                             <br/><br/>
                             Entre os mais usados estão:
                             <br/>
-                            <ul className="list-disc list-inside pl-2 flex flex-col gap-2 md:gap-1">
+                            <ul className="list-disc list-inside pl-2 flex flex-col gap-0.5 md:gap-1">
+                                <li>MyFitnessPal</li>
+                                <li>Yazio</li>
+                                <li>FatSecret</li>
+                                <li>HealthifyMe</li>
+                            </ul>
+                            <br/>
+                            Com eles, é possível escanear alimentos, salvar receitas e montar seu próprio diário alimentar.
+                        </p>
+                    </div>
+
+                    {/* 3 PILARES DA ALIMENTAÇÃO */}
+                    <div className="px-2 pt-1 pb-2 flex flex-col gap-3 rounded-lg  md:px-4 md:py-2 md:flex md:flex-col md:gap-5 md:rounded-xl bg-[#E0E0E0]">
+                        <div className="flex flex-col">
+                            <h2 className="text-lg md:text-2xl font-bold">Os 3 pilares da sua alimentação<span className="hidden md:block">: Proteínas, Carboidratos e Gorduras</span></h2>
+                            <span className="text-base hidden md:block">
+                                Eles não são vilões — são o combustível que mantém seu corpo funcionando todos os dias. Descubra como 
+                                cada um impacta sua energia, saúde e resultados.
+                            </span>
+                        </div>
+                        <div className="">
+                            <span className="text-base font-semibold">
+                                ⚡ Carboidratos:A fonte de energia preferida do organismo.
+                            </span>
+                            <ul className="ml-4">
+                                <li className="list-disc ml-4">Garantem disposição no dia a dia</li>
+                                <li className="list-disc ml-4">Melhoram desempenho físico e mental</li>
+                                <li className="list-disc ml-4">São o “combustível rápido” do corpo</li>
+                            </ul>
+                        </div>
+                        <div className="">
+                            <span className="text-base font-semibold">
+                                💪 Proteínas:O tijolo da construção do corpo.
+                            </span>
+                            <ul className="ml-4">
+                                <li className="list-disc ml-4">Constroem músculos e tecidos</li>
+                                <li className="list-disc ml-4">Auxiliam na recuperação e saciedade</li>
+                                <li className="list-disc ml-4">São essenciais para hormônios e imunidade</li>
+                            </ul>
+                        </div>
+                        <div className="">
+                            <span className="text-base font-semibold">
+                                🥑 Gorduras boas:Muito mais do que calorias extras.
+                            </span>
+                            <ul className="ml-4">
+                                <li className="list-disc ml-4">Regulam hormônios e cérebro</li>
+                                <li className="list-disc ml-4">Protegem órgãos e células</li>
+                                <li className="list-disc ml-4">Fornecem energia de longa duração</li>
+                            </ul>
+                        </div>
+                        <div className="flex flex-col mt-2 md:m-0 gap-3 lg:gap-2">
+                            <span className="text-base font-normal md:font-semibold">Quer entender como equilibrar cada macronutriente na sua dieta e usá-los a seu favor?</span>
+                            <span className="w-fit rounded-lg bg-[#121224] px-2 py-1 text-[#e7e7e7]">
+                                <a href="/macronutrientes" title="Página em construção">Descubra mais aqui</a>
+                            </span>
+                        </div>
+                    </div>
+
+                    {/* COMO CALCULAR COMIDA */}
+                    <div className="hidden px-2 pt-1 pb-2 rounded-lg  md:px-2 md:py-2 md:rounded-xl bg-[#E0E0E0]">
+                        <span className="w-full text-xl md:text-2xl font-semibold font-sans mb:2 md:mb-1"> Como saber quanto estou comendo? </span>
+                        <p className="w-full h-auto mb-4">
+                            Existem aplicativos que facilitam o controle da alimentação diária. Com eles, você pode buscar alimentos, 
+                            escanear rótulos, salvar receitas e registrar tudo o que come ao longo do dia.
+                            <br/><br/>
+                            Entre os mais usados estão:
+                            <br/>
+                            <ul className="list-disc list-inside pl-2 flex flex-col gap-0.5 md:gap-1">
                                 <li>MyFitnessPal</li>
                                 <li>Yazio</li>
                                 <li>FatSecret</li>
@@ -102,7 +231,7 @@ export default function Home(){
 
                     {/* MACRONUTRIENTES */}
 
-                    <div className="px-2 py-1 rounded-lg md:flex md:flex-col md:px-2 md:py-2 md:rounded-xl bg-[#E0E0E0]">
+                    <div className="hidden px-2 py-1 rounded-lg md:flex md:flex-col md:px-2 md:py-2 md:rounded-xl bg-[#E0E0E0]">
                         <span className="w-full text-xl md:text-2xl font-semibold font-sans mb-0.5 md:mb-2"> O que são macronutrientes? </span>
                         <p className="w-full h-auto mb-4 flex flex-col gap-4">
                             <span>
